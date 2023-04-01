18b7e345dbd33b82d4ba605e98ad265d7527314e
FireFading <fire_fading@mail.ru>
2023-04-02 02:50:54 +0300

    add noxfile

---
+++ noxfile.py
@@ -0,0 +1,34 @@
+import nox
+from dotenv import load_dotenv
+
+
+@nox.session
+def format(session: nox.Session) -> None:
+    session.install("ufmt", "black", "ruff")
+    session.run("ufmt", "format", ".")
+    session.run("black", "--config=configs/.black.toml", "app")
+    session.run(
+        "ruff",
+        "check",
+        "--config=configs/.ruff.toml",
+        "--fix",
+        "app",
+    )
+
+
+@nox.session
+def lint(session: nox.Session) -> None:
+    session.install("flake8", "mypy")
+    session.run("flake8", "--config=configs/.flake8", "app")
+    # session.run(
+    #     "mypy",
+    #     "--config-file=configs/.mypy.ini",
+    #     "app"
+    # )
+
+
+@nox.session
+def run_tests(session: nox.Session) -> None:
+    load_dotenv(dotenv_path="./.env.example")
+    session.install("-r", "requirements.txt")
+    session.run("pytest", ".")
